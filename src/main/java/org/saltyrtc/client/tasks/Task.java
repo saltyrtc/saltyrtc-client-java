e240b81917a55c5e13250e7082fbdbdc2e323298
Danilo Bargen <danilo.bargen@threema.ch>
2016-09-27 18:12:34 +0200

    Add a Task interface

---
+++ src/main/java/org/saltyrtc/client/tasks/Task.java
@@ -0,0 +1,29 @@
+package org.saltyrtc.client.tasks;
+
+import java.util.Map;
+
+/**
+ * A SaltyRTC task is a protocol extension to this protocol that will be negotiated during the
+ * client-to-client authentication phase. Once a task has been negotiated and the authentication is
+ * complete, the task protocol defines further procedures, messages, etc.
+ *
+ * All tasks need to implement this interface.
+ */
+public interface Task {
+
+    /**
+     * This method is called by SaltyRTC when a task related message
+     * arrives through the WebSocket.
+     *
+     * @param message The deserialized MessagePack message.
+     */
+    void onTaskMessage(Map<String, Object> message);
+
+	/**
+     * Send bytes through the task signaling channel.
+     *
+     * This method should only be called after the handover.
+     */
+    void sendSignalingMessage(byte[] payload);
+
+}
