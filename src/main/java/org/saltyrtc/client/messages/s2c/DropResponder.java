42d1d2ce761797356f3c8106b406829ba3efb233
Danilo Bargen <danilo.bargen@threema.ch>
2016-10-26 15:01:17 +0200

    Add reason field to drop-responder message

--- src/main/java/org/saltyrtc/client/messages/s2c/DropResponder.java
+++ src/main/java/org/saltyrtc/client/messages/s2c/DropResponder.java
@@ -9,43 +9,79 @@
 package org.saltyrtc.client.messages.s2c;
 
 import org.msgpack.core.MessagePacker;
+import org.saltyrtc.client.annotations.NonNull;
+import org.saltyrtc.client.annotations.Nullable;
 import org.saltyrtc.client.exceptions.ValidationError;
 import org.saltyrtc.client.helpers.ValidationHelper;
 import org.saltyrtc.client.messages.Message;
+import org.saltyrtc.client.signaling.CloseCode;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class DropResponder extends Message {
 
     public static final String TYPE = "drop-responder";
 
+    @NonNull
     private Integer id;
+    @Nullable
+    private Integer reason;
 
-    public DropResponder(Integer id) {
+    public DropResponder(@NonNull Integer id) {
         this.id = id;
+    }
+
+    public DropResponder(@NonNull Integer id, @Nullable Integer reason) {
+        this(id);
+        this.reason = reason;
     }
 
     public DropResponder(short id) {
         this.id = (int) id;
     }
 
+    public DropResponder(short id, @Nullable Integer reason) {
+        this(id);
+        this.reason = reason;
+    }
+
     public DropResponder(Map<String, Object> map) throws ValidationError {
         ValidationHelper.validateType(map.get("type"), TYPE);
         this.id = ValidationHelper.validateInteger(map.get("id"), 0x00, 0xff, "id");
+        if (map.containsKey("reason")) {
+            List<Integer> validRange = new ArrayList<>();
+            for (int i = 0; i < CloseCode.CLOSE_CODES_DROP_RESPONDER.length; i++) {
+                validRange.add(CloseCode.CLOSE_CODES_DROP_RESPONDER[i]);
+            }
+            this.reason = ValidationHelper.validateInteger(map.get("reason"), validRange, "reason");
+        }
     }
 
+    @NonNull
     public Integer getId() {
         return id;
     }
 
+    @Nullable
+    public Integer getReason() {
+        return reason;
+    }
+
     @Override
     public void write(MessagePacker packer) throws IOException {
-        packer.packMapHeader(2)
+        final boolean hasReason = this.reason != null;
+        packer.packMapHeader(hasReason ? 3 : 2)
                 .packString("type")
                     .packString(TYPE)
                 .packString("id")
                     .packInt(this.id);
+        if (hasReason) {
+            packer.packString("reason")
+                .packInt(this.reason);
+        }
     }
 
     @Override
