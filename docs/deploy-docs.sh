1b621752bfed9c449f67bf85a4f62e9724a68267
Danilo Bargen <mail@dbrgn.ch>
2017-11-16 13:43:13 +0100

    Switch to CircleCI

--- docs/deploy-docs.sh
+++ docs/deploy-docs.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
+set -euo pipefail
+
+echo "Preparing docs"
+
+echo "-> Creating venv..."
 cd docs
-virtualenv --no-site-packages VENV
+python3 -m venv VENV
 VENV/bin/pip install -r requirements.txt
+
+echo "-> Building docs..."
 VENV/bin/mkdocs build -v --clean
 
-git config user.name "Travis CI"
-git config user.email "travis@saltyrtc.org"
+echo "-> Git config..."
+git config user.name "Circle CI"
+git config user.email "ci@saltyrtc.org"
 git remote add gh-token "https://${GH_TOKEN}@github.com/saltyrtc/saltyrtc-client-java.git"
 git fetch gh-token && git fetch gh-token gh-pages:gh-pages
 
+echo "-> Publish docs to Github pages"
 VENV/bin/mkdocs gh-deploy -v --clean --remote-name gh-token
